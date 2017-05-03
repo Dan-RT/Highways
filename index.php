716149821e943c2113c3b32f845e07d542c64e10
Dan-RT <daniel.regnard@efrei.net>
2017-05-03 12:19:51 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8" />
+    <title>BEER'SPOT</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="CSS/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+    <body>
+
+
+        <div class="container-fullwidth container-fluid">
+            <div class="row">
+
+                <div>TEST </div>
+
+            </div>
+        </div>
+
+    </body>
+</html>
