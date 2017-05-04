2b187464fa951603c868c4ca22006cd8116f8e8c
Dan-RT <daniel.regnard@efrei.net>
2017-05-04 22:20:20 +0200

    Select pour l'ajout d'autoroute fait, jquery pour vérifier les données du formulaire également (parfois certains bugs avec le double choix d'une même ville), il faut faire les modif vers la BDD maintenant

--- add_element.php
+++ add_element.php
@@ -1,7 +1,8 @@
 <?php
 
+    include("database.php");
     $element = $_GET['element'];
-    //echo $element;
+    echo $element;
 
     switch ($element) {
 
@@ -9,46 +10,76 @@
 
 ?>
 
-    <div class="row">
-        <div class="col-lg-6">
-            <h4>Nom de l'Autoroute : </h4>
-        </div>
-        <label><input class="input-md form-control" type="text" name="name_highway" placeholder="Ex : A1"/></label>
-    </div>
+    <div id="add_section">
 
-    <div class="row">
+        <input id="data_element" name="data_element" value="<?php echo $element; ?>">
 
-        <div class="col-lg-6">
-            <h4>Ville de départ : </h4>
+        <div class="row">
+            <div class="col-lg-6">
+                <h4>Nom de l'Autoroute : </h4>
+            </div>
+            <label><input class="input-md form-control" type="text" name="name_highway" placeholder="Ex : A1"/></label>
         </div>
-        <div class="col-lg-6">
-            <select name="starting_city" id="starting_city">
-                <option value="1">data missing</option>
-                <option value="2">data missing</option>
-                <option value="3">data missing</option>
-                <option value="4">data missing</option>
-                <option value="5">data missing</option>
-                <option value="6">data missing</option>
-            </select>
-        </div>
-    </div>
 
-    <div class="row">
+        <div class="row">
+            <div class="col-lg-6">
+                <h4>Ville de départ : </h4>
+            </div>
 
-        <div class="col-lg-6">
-            <h4>Ville d'arrivée : </h4>
+            <div class="col-lg-6">
+                <select name="starting_city" id="starting_city">
+                    <option value="0">Sélectionner une ville</option>
+<?php
+
+            $reponse = query_database('SELECT * FROM Ville');
+
+
+            //$arriving_city = $reponse;
+            $i = 1;
+
+            while ($starting_city = $reponse->fetch()) {
+?>
+                    <option value="<?php echo $i; ?>"><?php echo $starting_city['Nom_ville']; ?></option>
+
+    <?php
+                    $i++;
+                 }
+
+    ?>
+                </select>
+            </div>
         </div>
-        <div class="col-lg-6">
-            <select name="arriving_city" id="arriving_city">
-                <option value="1">data missing</option>
-                <option value="2">data missing</option>
-                <option value="3">data missing</option>
-                <option value="4">data missing</option>
-                <option value="5">data missing</option>
-                <option value="6">data missing</option>
-            </select>
+
+
+
+
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h4>Ville d'arrivée : </h4>
+            </div>
+            <div class="col-lg-6">
+                <select name="arriving_city" id="arriving_city">
+                    <option value="0">Sélectionner une ville</option>
+
+        <?php
+                $reponse = query_database('SELECT * FROM Ville');
+                $i = 1;
+
+                while ($arriving_city = $reponse->fetch()) {
+                    ?>
+                    <option value="<?php echo $i; ?>"><?php echo $arriving_city['Nom_ville']; ?></option>
+                    <?php
+                }
+        ?>
+                </select>
+            </div>
         </div>
-    </div>
+
+
+
+
+
 
 
 <?php
@@ -58,54 +89,55 @@
        case "city":
 ?>
 
-           <div class="row">
-               <div class="col-lg-6">
-                   <h4>Nom de la ville : </h4>
-               </div>
-               <label><input class="input-md form-control" type="text" name="name_city" placeholder="Ex: Paris"/></label>
+       <div class="row">
+           <div class="col-lg-6">
+               <h4>Nom de la ville : </h4>
            </div>
+           <label><input class="input-md form-control" type="text" name="name_city" placeholder="Ex: Paris"/></label>
+       </div>
 
-           <div class="row">
-               <div class="col-lg-6">
-                   <h4>Code Postal : </h4>
-               </div>
-               <label><input class="input-md form-control" type="text" name="postal_code" placeholder="Ex: 75001"/></label>
+
+       <div class="row">
+           <div class="col-lg-6">
+               <h4>Code Postal : </h4>
            </div>
+           <label><input class="input-md form-control" type="text" name="postal_code" placeholder="Ex: 75001"/></label>
+       </div>
 
 
-           <div class="row">
+       <div class="row">
 
-               <div class="col-lg-6">
-                   <h4>Autoroute :</h4>
-               </div>
-               <div class="col-lg-6">
-                   <select name="city_highway" id="city_highway">
-                       <option value="1">data missing</option>
-                       <option value="2">data missing</option>
-                       <option value="3">data missing</option>
-                       <option value="4">data missing</option>
-                       <option value="5">data missing</option>
-                       <option value="6">data missing</option>
-                   </select>
-               </div>
+           <div class="col-lg-6">
+               <h4>Autoroute :</h4>
            </div>
-
-           <div class="row">
-
-               <div class="col-lg-6">
-                   <h4>Tronçon :</h4>
-               </div>
-               <div class="col-lg-6">
-                   <select name="city_portion" id="city_portion">
-                       <option value="1">data missing</option>
-                       <option value="2">data missing</option>
-                       <option value="3">data missing</option>
-                       <option value="4">data missing</option>
-                       <option value="5">data missing</option>
-                       <option value="6">data missing</option>
-                   </select>
-               </div>
+           <div class="col-lg-6">
+               <select name="city_highway" id="city_highway">
+                   <option value="1">data missing</option>
+                   <option value="2">data missing</option>
+                   <option value="3">data missing</option>
+                   <option value="4">data missing</option>
+                   <option value="5">data missing</option>
+                   <option value="6">data missing</option>
+               </select>
            </div>
+       </div>
+
+       <div class="row">
+
+           <div class="col-lg-6">
+               <h4>Tronçon :</h4>
+           </div>
+           <div class="col-lg-6">
+               <select name="city_portion" id="city_portion">
+                   <option value="1">data missing</option>
+                   <option value="2">data missing</option>
+                   <option value="3">data missing</option>
+                   <option value="4">data missing</option>
+                   <option value="5">data missing</option>
+                   <option value="6">data missing</option>
+               </select>
+           </div>
+       </div>
 
 
 <?php
@@ -114,94 +146,92 @@
        case "portion":
 ?>
 
+       <div class="row">
+           <div class="col-lg-6">
+               <h4>Code du tronçon : </h4>
+           </div>
+           <label><input class="input-md form-control" type="text" name="portion_code" placeholder="Ex: XXX1"/></label>
+       </div>
+
+       <div class="row">
+           <div class="col-lg-6">
+               <h4>Km de départ : </h4>
+           </div>
+           <label><input class="input-md form-control" type="text" name="starting_km" placeholder="Ex: 65"/></label>
+       </div>
+
+       <div class="row">
+           <div class="col-lg-6">
+               <h4>Km d'arrivée : </h4>
+           </div>
+           <label><input class="input-md form-control" type="text" name="arriving_km" placeholder="Ex: 258"/></label>
+       </div>
+
+
+
+       <div class="row">
+
+           <div class="col-lg-6">
+               <h4>Autoroute :</h4>
+           </div>
+           <div class="col-lg-6">
+               <select name="portion_highway" id="portion_highway">
+                   <option value="1">data missing</option>
+                   <option value="2">data missing</option>
+                   <option value="3">data missing</option>
+                   <option value="4">data missing</option>
+                   <option value="5">data missing</option>
+                   <option value="6">data missing</option>
+               </select>
+           </div>
+       </div>
+
+
+       <div class="row">
+           <div class="col-lg-offset-3 col-lg-3">
+               <input type="radio" name="opened" value="1" checked> Ouvert<br>
+           </div>
+           <div class="col-lg-3">
+               <input type="radio" name="opened" value="2"> Fermé<br>
+           </div>
+       </div>
+
+       <div id="closed_part">
            <div class="row">
                <div class="col-lg-6">
-                   <h4>Code du tronçon : </h4>
+                   <h4>Causes : </h4>
                </div>
-               <label><input class="input-md form-control" type="text" name="portion_code" placeholder="Ex: XXX1"/></label>
+               <label><input class="input-md form-control" type="text" name="reason_closed" placeholder="Travaux"/></label>
            </div>
+       </div>
 
-           <div class="row">
-               <div class="col-lg-6">
-                   <h4>Km de départ : </h4>
-               </div>
-               <label><input class="input-md form-control" type="text" name="starting_km" placeholder="Ex: 65"/></label>
+
+       <div class="row">
+           <div class="col-lg-offset-3 col-lg-3">
+               <input type="radio" name="paid" value="1" checked> Gratuit<br>
            </div>
-
-           <div class="row">
-               <div class="col-lg-6">
-                   <h4>Km d'arrivée : </h4>
-               </div>
-               <label><input class="input-md form-control" type="text" name="arriving_km" placeholder="Ex: 258"/></label>
+           <div class="col-lg-3">
+               <input type="radio" name="paid" value="2"> Payant<br>
            </div>
+       </div>
 
 
 
-           <div class="row">
-
-               <div class="col-lg-6">
-                   <h4>Autoroute :</h4>
-               </div>
-               <div class="col-lg-6">
-                   <select name="portion_highway" id="portion_highway">
-                       <option value="1">data missing</option>
-                       <option value="2">data missing</option>
-                       <option value="3">data missing</option>
-                       <option value="4">data missing</option>
-                       <option value="5">data missing</option>
-                       <option value="6">data missing</option>
-                   </select>
-               </div>
-           </div>
-
-
-           <div class="row">
-               <div class="col-lg-offset-3 col-lg-3">
-                   <input type="radio" name="opened" value="1" checked> Ouvert<br>
-               </div>
-               <div class="col-lg-3">
-                   <input type="radio" name="opened" value="2"> Fermé<br>
-               </div>
-           </div>
-
-           <div id="closed_part">
-               <div class="row">
-                   <div class="col-lg-6">
-                       <h4>Causes : </h4>
-                   </div>
-                   <label><input class="input-md form-control" type="text" name="reason_closed" placeholder="Travaux"/></label>
-               </div>
-           </div>
-
-
-           <div class="row">
-               <div class="col-lg-offset-3 col-lg-3">
-                   <input type="radio" name="paid" value="1" checked> Gratuit<br>
-               </div>
-               <div class="col-lg-3">
-                   <input type="radio" name="paid" value="2"> Payant<br>
-               </div>
-           </div>
-
-
-
-            <div id="paid_part">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <h4>Payant du Km : </h4>
-                    </div>
-                    <label><input class="input-md form-control" type="text" name="paid_starting_km" placeholder="Ex: 26"/></label>
+        <div id="paid_part">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h4>Payant du Km : </h4>
                 </div>
-
-                <div class="row">
-                    <div class="col-lg-6">
-                        <h4>Jusqu'au Km : </h4>
-                    </div>
-                    <label><input class="input-md form-control" type="text" name="paid_arriving_km" placeholder="Ex: 129"/></label>
-                </div>
+                <label><input class="input-md form-control" type="text" name="paid_starting_km" placeholder="Ex: 26"/></label>
             </div>
 
-
+            <div class="row">
+                <div class="col-lg-6">
+                    <h4>Jusqu'au Km : </h4>
+                </div>
+                <label><input class="input-md form-control" type="text" name="paid_arriving_km" placeholder="Ex: 129"/></label>
+            </div>
+        </div>
 
 <?php
            break;
@@ -209,9 +239,10 @@
 
 ?>
 
-            <button id="add_button" class="btn btn-md btn-default submit_button" >
-                <span class="glyphicon glyphicon-ok"></span> Ajouter
-            </button>
+        <button id="add_button" class="btn btn-md btn-default submit_button" >
+            <span class="glyphicon glyphicon-ok"></span> Ajouter
+        </button>
 
+        <script src="js/add_element.js"></script>
 
-            <script src="js/add_element.js"></script>
+    </div>
