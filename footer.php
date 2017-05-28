37d83c4f148fbceb2823bd465bb4c82cc82cca23
Dan-RT <daniel.regnard@efrei.net>
2017-05-28 15:05:09 +0200

    bug fixed

--- footer.php
+++ footer.php
@@ -3,6 +3,6 @@
     <ul class="footer_list">
         <li>May 2017<p class="tab">|</p> </li>
         <li>Daniel REGNARD<p class="tab">|</p></li>
-        <li><!--Emna BOUJEMAA --> Lorem Ipsum<p class="tab"></p></li>
+        <li>Emna BOUJEMAA<p class="tab"></p></li>
     </ul>
 </footer>
